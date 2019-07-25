b216dce1b1e47769c5df2d3b19b7add6257714b7
Charlie McCabe <charlie102m@gmail.com>
2019-07-25 21:27:25 +0100

    comit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,6 @@
 <template>
   <div id="app">
     <v-app>
-      <!-- <v-app>
-        <v-navigation-drawer>
-        </v-navigation-drawer>
-        <v-toolbar app></v-toolbar>
-        <v-content>
-          <v-container fluid>
-            <router-view></router-view>
-          </v-container>
-        </v-content>
-        <v-footer app></v-footer>
-      </v-app> -->
       <v-layout fill-height>
         <v-flex>
           <SideNav/>
