fdc6888226aca3452ce81b06f622274efd877bf7
Charlie McCabe <charlie102m@gmail.com>
2019-08-07 16:15:45 +0100

    comit

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,16 @@
 const mysql = require('mysql')
 
 if (process.env.NODE_ENV === 'production') {
-    
+    module.exports ={
+        connection: mysql.createConnection({
+            host: 'c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'ebvwv8qmyj5z29m9',
+            password: 'me3qgdpcts4ti79m',
+            port: '3306',
+            database: 'jxa8oq1hxejjpr7c',
+            multipleStatements: true
+        })
+    }
 } else { 
     module.exports ={
         connection: mysql.createConnection({
