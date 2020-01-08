555624d11b72f5247b55e5849405849fd65a6630
Charlie McCabe <charlie102m@gmail.com>
2020-01-08 13:08:01 +0000

    reference req.params instead of req.body for expense id when updating expense

--- server/controllers/ExpensesController.js
+++ server/controllers/ExpensesController.js
@@ -61,7 +61,7 @@ const controller = {
         let query = `UPDATE expenses
                         SET ?
                         WHERE id = ? AND createdBy = ?`
-        let data = [req.body, req.body.id, req.headers.user.id]
+        let data = [req.body, req.params.expenseId, req.headers.user.id]
         connection.query(query, data, (error, results) => {
             if (error) return res.status(403).send(error.sqlMessage)
             return res.send(results)
